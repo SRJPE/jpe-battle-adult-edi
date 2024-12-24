3b656f5a78429897a6af415e2384c3559fc63b3d
Badhia <byuneskatz@flowwest.com>
2024-12-23 16:03:48 -0800

    adds notes and todos to keep track of work done

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -262,11 +262,16 @@ redd_2022_2023_data <- clean_2022_2023_data |>
            TRUE ~ NA))
 
 # checked for NA's. None were introduced while binding
-# TODO note to self. I left off here, I have combined all years of redd data. next steps - check that no NA's were introduced.
-# then run substrate categorization below (checking that ranges are the same)
-# standardize substrate sizes for redd using the Wentworth Scale, created by W.C Krumbein
-# when the size range fell into two categories, they were rounded down
+# TODO note to self. I left off here, up until this point data is clean:
 
+# I have combined all years of redd data. checked that no NA's were introduced.
+# substrate size ranges were cleaned.
+
+# Next steps: figure out how to fit into run substrate categorization below, that criteria was used in past data (ranges are not the same in this new data)
+# aka standardize substrate sizes for redd using the Wentworth Scale, created by W.C Krumbein
+# note: when the size range fell into two categories, they were rounded down
+
+# code below was already here from last update (keeping for reference)---
 # standarized size ranges lookup
 substrate_class = data.frame("standardized_size_range" = c("<0.25",
                                                            "0.25-0.5",
@@ -286,7 +291,6 @@ substrate_class = data.frame("standardized_size_range" = c("<0.25",
 #for NAs. Before joining, filter out 2022 from original data
 unique(redd_2022_2023_data$redd_substrate_size)
 
- # TODO check ranges
 redd_substrate_size_lookup <-
   data.frame("redd_substrate_size" = unique(redd_2022_2023_data$redd_substrate_size),
              "standardized_size_range" = c(NA, "1-2", "1-2", "2-4", "2-4",
@@ -294,44 +298,44 @@ redd_substrate_size_lookup <-
                                            "1-2", "1-2", "4-8", "<0.25", "1-2",
                                            "2-4", "0.5-1", "1-2", "2-4", "2-4",
                                            "<0.25", "4-8", "4-8")) |>
-  left_join(substrate_class)
+  left_join(substrate_class) # currently getting an error here
 
-redd <- redd_raw |>
-  mutate(survey_method = str_to_lower(survey_method),
-         # add redd count for all rows (each row is an observation)
-         # to avoid double counting, you need to take the unique redd_id
-         redd_count = 1) |>
-  left_join(redd_substrate_size_lookup |>
-              select(redd_substrate_size, redd_substrate_class),
-            by = c("redd_substrate_size")) |>
-  left_join(redd_substrate_size_lookup |>
-              select(redd_substrate_size, tail_substrate_class = redd_substrate_class),
-            by = c("tail_substrate_size" = "redd_substrate_size")) |>
-  left_join(redd_substrate_size_lookup |>
-              select(redd_substrate_size, pre_redd_substrate_class = redd_substrate_class),
-            by = c("pre_redd_substrate_size" = "redd_substrate_size")) |>
-  mutate(reach = case_when(reach == "4" ~ "R4",
-                           reach == "1" ~ "R1",
-                           reach == "2" ~ "R2",
-                           reach == "5" ~ "R5",
-                           reach == "3" ~ "R3",
-                           reach == "R12" ~ "R1",
-                           TRUE ~ reach),
-         # Reach 1B began being surveyed in 2021 due to private land concerns
-         reach = ifelse(year(date) >= 2021 & reach == "R1", "R1B", reach)) |>
-  select(date, redd_id = JPE_redd_id, reach, fish_on_redd = fish_guarding, age, run, redd_count,
-         redd_measured, redd_width, redd_length,
-         pre_redd_depth, redd_pit_depth, redd_tail_depth,
-         redd_substrate_class, tail_substrate_class, pre_redd_substrate_class,
-         velocity = flow_fps)
+# redd <- redd_raw |>
+#   mutate(survey_method = str_to_lower(survey_method),
+#          # add redd count for all rows (each row is an observation)
+#          # to avoid double counting, you need to take the unique redd_id
+#          redd_count = 1) |>
+#   left_join(redd_substrate_size_lookup |>
+#               select(redd_substrate_size, redd_substrate_class),
+#             by = c("redd_substrate_size")) |>
+#   left_join(redd_substrate_size_lookup |>
+#               select(redd_substrate_size, tail_substrate_class = redd_substrate_class),
+#             by = c("tail_substrate_size" = "redd_substrate_size")) |>
+#   left_join(redd_substrate_size_lookup |>
+#               select(redd_substrate_size, pre_redd_substrate_class = redd_substrate_class),
+#             by = c("pre_redd_substrate_size" = "redd_substrate_size")) |>
+#   mutate(reach = case_when(reach == "4" ~ "R4",
+#                            reach == "1" ~ "R1",
+#                            reach == "2" ~ "R2",
+#                            reach == "5" ~ "R5",
+#                            reach == "3" ~ "R3",
+#                            reach == "R12" ~ "R1",
+#                            TRUE ~ reach),
+#          # Reach 1B began being surveyed in 2021 due to private land concerns
+#          reach = ifelse(year(date) >= 2021 & reach == "R1", "R1B", reach)) |>
+#   select(date, redd_id = JPE_redd_id, reach, fish_on_redd = fish_guarding, age, run, redd_count,
+#          redd_measured, redd_width, redd_length,
+#          pre_redd_depth, redd_pit_depth, redd_tail_depth,
+#          redd_substrate_class, tail_substrate_class, pre_redd_substrate_class,
+#          velocity = flow_fps)
 
 # the goal of this summary is that we show data without changing to pivot longer
-redd_summary <- redd |>
-  mutate(year = year(date)) |>
-  group_by(year) |>
-  distinct(redd_id, .keep_all = T) |>
-  summarize(total_annual_redd_count = sum(redd_count),
-            number_reaches_surveyed = length(unique(reach)))
+# redd_summary <- redd |>
+#   mutate(year = year(date)) |>
+#   group_by(year) |>
+#   distinct(redd_id, .keep_all = T) |>
+#   summarize(total_annual_redd_count = sum(redd_count),
+#             number_reaches_surveyed = length(unique(reach)))
 
 
 # upstream passage --------------------------------------------------------
